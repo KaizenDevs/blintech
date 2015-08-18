2a63de9b37fbf12154a13f03c811401f9b5bb834
German Hernandez <muygrafico@gmail.com>
2015-08-18 13:58:58 -0500

    add actions and views company, shield, rent, maintenance, news, contact to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def company
+  end
+
+  def shield
+  end
+
+  def rent
+  end
+
+  def maintenance
+  end
+
+  def news
+  end
+
+  def contact
+  end
 end
